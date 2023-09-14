1c0a28a06487b13e83d3ec995090aa9f0e900d5f
KMBA-Kaiser <veliliakaizer12345@gmail.com>
2023-09-14 16:34:21 +0800

    first push

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+	def landing
+    # You can add additional logic here if needed
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root 'pages#landing'
+  get "/events", to: "events#index"
+  get "/events/new/:id", to: "events#new"
+  resources :events
+end

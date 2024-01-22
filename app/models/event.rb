fad8807d4f4114b65a1c7c4925c757c80979b242
KMBA-Kaiser <veliliakaizer12345@gmail.com>
2024-01-22 13:25:15 +0800

    add adminlte

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,9 @@ class Event < ApplicationRecord
 	# has_many :reservations
 	# has_many :users, through: :reservations
 	belongs_to :venue
+	# validate :date_onwards
 	PAYMENT_TYPE = ["Cash", "G-Cash", "Bank-Transfer"]
+
 	def to_s
 		name
 	end
@@ -10,4 +12,12 @@ class Event < ApplicationRecord
 	def venue_name
 		self.venue.name
 	end
+
+	# def date_onwards
+	#     return if date.blank?
+	#     	if date < Date.today
+	#       		errors.add(:date, "Sorry Date is already past")
+	#     end
+ 	# end
+
 end
